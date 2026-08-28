eeb0613d058d7e36521a66a766fdefd0ec9b99e9
bethkimani <kbwaceke@gmail.com>
2026-08-28 09:31:44 +0300

    added function in python

---
+++ operator.py
@@ -0,0 +1,23 @@
+a = 20
+b = 6
+
+# Addition
+print(a + b)
+
+# Subtraction
+print(a - b)
+
+# Multiplication
+print(a * b)
+
+# Division
+print(a / b)
+
+# Floor Division
+print(a // b)
+
+# Modulus (remainder)
+print(a % b)
+
+# Exponent
+print(a ** b)
